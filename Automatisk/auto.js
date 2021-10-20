773eeb6009633b80e1b7337e15c4ec0f8edcf7d7
Audun Hauge <audun.hauge@gmail.com>
2021-10-20 19:15:16 +0200

    fixers

--- Automatisk/auto.js
+++ Automatisk/auto.js
@@ -1,20 +1,27 @@
 // @ts-check
 
 import {updateMyProperties, thingsWithId, prepTheWebPage} from "./Minos.js";
-
-const {main} = thingsWithId();
+// må kjøre prep før vi henter ting med id
+// bytter ut {navn} med span
 prepTheWebPage();
+// henter de tingene vi har gitt id på websida
+const {pris,kjop} = thingsWithId();
+
 
 const mineTing = updateMyProperties(
     {
         antall:0,
+        poeng:10,
+        total:0,
     });
 
-main.addEventListener("click", () => {
+kjop.addEventListener("click", () => {
     mineTing.antall += 1;
+    mineTing.poeng -= 1;
+    mineTing.total = Number(pris.value) * mineTing.antall;
 });
 
 mineTing.antall = 0;
+mineTing.poeng = 10.1;
 
-export { mineTing};
 
