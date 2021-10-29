a05f1e252e1f9c469c724b0f2d88756c0cf14680
Audun Hauge <audun.hauge@gmail.com>
2021-10-29 13:25:25 +0200

    fixl

--- FilmRegister/film.js
+++ FilmRegister/film.js
@@ -2,9 +2,46 @@
 import { updateMyProperties, thingsWithId } from "../lib/Minos.js";
 
 const web = updateMyProperties();
-/** web vil ha en web.xx og web.zz   gitt: <div>{xx} <input name="zz"> {zz} </div>
- * disse vil automatisk oppdateres, web.xx = 1 => {xx} erstattes med 1
- * endring av input oppdaterer web.zz og {zz} (og omvendt)
- */
+const { liste, tittel } = thingsWithId();
 
-const { /** ting på websida med id */ } = thingsWithId();
+const tittelListe = web.getLocalJSON("filmer") || [];
+const fjernGammelTekst = () => {
+    web.tittel = "";
+    web.regissor = "";
+};
+const setFocusItittel = () => tittel.focus();
+const visAlletittler = () => web.filmene = web.wrap(tittelListe, "li");
+const lagreDataPermanent = () => web.setLocalJSON("filmer", tittelListe);
+
+tittel.focus();
+visAlletittler();
+
+const registrerNytittel = () => {
+    const nytittel = web.tittel;
+    const nyregissor = web.regissor;
+    setFocusItittel();
+    fjernGammelTekst();
+    if (nytittel === "") {
+        return;
+    }
+    tittelListe.push(nytittel + " " + nyregissor);
+    visAlletittler();
+    lagreDataPermanent();
+}
+
+regissor.onkeypress = e => e.key === "Enter" ? registrerNytittel() : null;
+
+liste.onclick = (e) => {
+    const t = e.target;
+    const txt = t.innerText;
+    const idx = tittelListe.indexOf(txt);
+    if (idx >= 0) {
+        const siste = tittelListe.pop();
+        if (tittelListe.length > idx) {
+            tittelListe[idx] = siste;
+        }
+        visAlletittler();
+        lagreDataPermanent();
+    }
+}
+
