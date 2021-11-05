ae8450ba181a365277494ebf78a786064edc3bef
Audun Hauge <audun.hauge@gmail.com>
2021-11-05 11:29:58 +0100

    improved

--- Pizza/pizza.js
+++ Pizza/pizza.js
@@ -1,5 +1,6 @@
 // @ts-check
-import { updateMyProperties, thingsWithId } from "../lib/Minos.js";
+import { updateMyProperties, thingsWithId, preCalc } from "../lib/Minos.js";
+const {random} = Math;
 
 const web = updateMyProperties();
 /** web vil ha en web.xx og web.zz   gitt: <div>{xx} <input name="zz"> {zz} </div>
@@ -7,4 +8,33 @@ const web = updateMyProperties();
  * endring av input oppdaterer web.zz og {zz} (og omvendt)
  */
 
-const { /** ting på websida med id */ } = thingsWithId();
+const { kjop,bynavn } = thingsWithId();
+
+preCalc();  // eventuelle beregninger som {3+4}
+// de som kan beregnes uten brukerinput oppdateres nå
+
+const byliste = "haugesund,bergen,os".split(",");
+const pizzaliste = [
+    {pizza:"drengen",pris:120},
+    {pizza:"diavolo",pris:220},
+    {pizza:"marguerita",pris:90},
+];
+
+bynavn.onkeyup = () => {
+   const fu = byliste.find(b => b.includes(web.bynavn.toLowerCase()));
+   if (fu) {
+       web.funnet = fu;
+   }
+}
+
+pizz.onkeyup = () => {
+    const fu = pizzaliste.find(p => p.pizza.includes(web.pizz.toLowerCase()));
+    if (fu) {
+        web.valgt = fu.pizza;
+        web.pris = fu.pris;
+    }
+ }
+
+kjop.onclick = () => {
+    web.handlekorg.push( {pizza:web.valgt, pris:web.pris} );
+}
