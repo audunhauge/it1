1ae65558e58eee7261fead11c4c7b295ff64dc53
Audun Hauge <audun.hauge@gmail.com>
2021-11-19 12:56:58 +0100

    numbers

--- Addisjon/add.js
+++ Addisjon/add.js
@@ -3,14 +3,38 @@ import { updateMyProperties, thingsWithId } from "../lib/Minos.js";
 
 const web = updateMyProperties();
 
+web.pos = 0;
+
+
 
 const { vis, algoritme, add } = thingsWithId();
 
 vis.onclick = () => {
+    if (Number(web.t1) > 0 && Number(web.t2) > 0) {
+        algoritme.style.opacity = "1";
+        web.siffer = 0;
+        const siffer1 = Array.from(web.t1).reverse();
+        const siffer2 = Array.from(web.t2).reverse();
+        web.n0 = siffer1[0] || "";
+        web.n1 = siffer1[1] || "";
+        web.n2 = siffer1[2] || "";
+        web.n3 = siffer1[3] || "";
+        web.n4 = siffer1[4] || "";
+        
+        web.m0 = siffer2[0] || "";
+        web.m1 = siffer2[1] || "";
+        web.m2 = siffer2[2] || "";
+        web.m3 = siffer2[3] || "";
+        web.m4 = siffer2[4] || "";
 
-    algoritme.style.opacity = "1";
+    }   
 }
 
 add.onclick = () => {
+    const s = web.siffer;
+    const nr1 = Number(web["n"+s]);  
+    const nr2 = Number(web["m"+s]);
+    const sum = nr1 + nr2;
+    web["s"+s] = sum;  
     web.siffer++;
 }
