9b10c7019cf7cbf165e749d324d0bd7c8733ddd1
Audun Hauge <audun.hauge@gmail.com>
2021-12-15 14:15:06 +0100

    siste push før jul

--- Julekalender/jul.js
+++ Julekalender/jul.js
@@ -3,30 +3,10 @@ import { updateMyProperties, thingsWithId, shuffle } from "../lib/Minos.js";
 
 const web = updateMyProperties();
 
-const juleTall = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]);
+const { timeplan } = thingsWithId();
 
-const bilder = [];
-for (let i=0; i< 25; i++) {
-    const p = 0x1F4c4;
-    bilder.push(String.fromCodePoint(p+i));
+for (let nr = 1; nr < 9; nr++) {
+    web.timeplan.push({ nr });
 }
 
-const { kalender, timeplan } = thingsWithId();
 
-for (let i = 0; i < 24; i++) {
-    const nr = juleTall[i];
-    web.kalender.push({ nr });
-}
-
-for (let i = 1; i < 9; i++) {
-    web.timeplan.push({ i });
-}
-
-kalender.onclick = (e) => {
-    const t = e.target;
-    if (t.className === "luke") {
-        const idx = t.dataset.nr;
-        t.innerHTML = bilder[idx];
-    }
-}
