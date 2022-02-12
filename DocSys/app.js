b69248fc8921bf9e2c502897788007fcfa00e36c
Audun Hauge <audun.hauge@gmail.com>
2022-02-12 15:56:22 +0100

    fix

--- DocSys/app.js
+++ DocSys/app.js
@@ -1,6 +1,16 @@
 // @ts-check
 
-import {velgFiler} from "./util.js";
+import {velgFiler,linjerMedOrd, lengsteLinje} from "./util.js";
 
-const minefiler = velgFiler("b.pdf c.pdf","doc");
-console.log(minefiler);
+console.log(velgFiler("b.pdf c.pdf c.doc","doc"));
+
+const tekst = `dette er
+noen linjer med og med
+flere ord og
+flere mellomrum og
+og my annet`;
+console.log(linjerMedOrd(tekst,"og"));
+
+console.log(lengsteLinje(tekst));
+console.log(lengsteLinje(""));
+console.log(lengsteLinje("1234567890123"));
--- DocSys/util.js
+++ DocSys/util.js
@@ -11,3 +11,27 @@ export function velgFiler(filer,type) {
     }
     return ret.join(" ");
 }
+
+export function linjerMedOrd(tekst,ord) {
+    const ret = [];
+    const liste = tekst.split("\n");
+    for (let i=0;i< liste.length; i++) {
+        const linje = liste[i];
+        if (linje.includes(ord)) {
+            ret.push(linje);
+        }
+    }
+    return ret.length;
+}
+
+export function lengsteLinje(tekst) {
+    let lengst = 0;
+    const liste = tekst.split("\n");
+    for (let i=0;i< liste.length; i++) {
+        const linje = liste[i];
+        if (linje.length > lengst) {
+            lengst = linje.length;
+        }
+    }
+    return lengst;
+}
